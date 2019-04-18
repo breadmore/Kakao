51267ef67cd5a7c8006a3ea5227f5ef5c4f6945c
KNT01 <lepo3@naver.com>
2019-04-18 21:42:45 +0900

    heroku operation test

--- public/dao/DateDao.js
+++ public/dao/DateDao.js
@@ -6,19 +6,22 @@ selectAllDate: function (callback) {
     return db.query('select CAL_DAY, state from jdDate',callback);
 },
 selectNightShift: function (callback) {
-        return db.query('select CAL_DAY from jdDate where state = \'야\' ;',callback);
+        return db.query('select CAL_DAY from jdDate where state = \'야\'',callback);
     },
 selectNightShiftB: function (callback) {
-        return db.query('select CAL_DAY from jdDate where state = \'야B\' ;',callback);
+        return db.query('select CAL_DAY from jdDate where state = \'야B\'',callback);
     },
 selectNonShift: function (callback) {
-        return db.query('select CAL_DAY from jdDate where state = \'비\' ;',callback);
+        return db.query('select CAL_DAY from jdDate where state = \'비\'',callback);
     },
 selectNonWork: function (callback) {
         return db.query('select CAL_DAY from jdDate where state = \'휴\' AND CAL_DAY>=(date_format(now(), \'%d\')+0)',callback);
     },
 selectDayWork: function (callback) {
-        return db.query('select CAL_DAY from jdDate where state = \'주\' ;',callback);
+        return db.query('select CAL_DAY from jdDate where state = \'주\'',callback);
+    },
+selectTodayWork: function (callback) {
+        return db.query('select CAL_DAY, state from jdDate',callback);
     }
 };
 
